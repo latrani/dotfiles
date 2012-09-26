89e7311c34ead0da5415391517dcc2f9fc6d2af3
Matt Thomas <matt+github@latrani.net>
2012-09-25 20:50:07 -0700

    Setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -si ~/.mydotfiles/ackrc ~/.ackrc
+ln -si ~/.mydotfiles/screenrc ~/.screenrc
+ln -si ~/.mydotfiles/zshrc ~/.zshrc
+ln -si ~/.mydotfiles/ssh-config ~/.ssh/config
+ln -si ~/.mydotfiles/latrani.zsh-theme ~/.oh-my-zsh/themes/latrani.zsh-theme

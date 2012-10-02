12918eced844637323b7887ad5382819926e0c79
Matt Thomas <matt.thomas@adjitsu.com>
2012-10-02 16:38:50 -0700

    Adding dircolors

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ ln -si ~/.mydotfiles/ackrc ~/.ackrc
 ln -si ~/.mydotfiles/screenrc ~/.screenrc
 ln -si ~/.mydotfiles/zshrc ~/.zshrc
 ln -si ~/.mydotfiles/ssh-config ~/.ssh/config
+ln -si ~/.mydotfiles/dircolors ~/.dircolors
 ln -si ~/.mydotfiles/latrani.zsh-theme ~/.oh-my-zsh/themes/latrani.zsh-theme
